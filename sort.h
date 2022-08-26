4f5273a219949a75ddca0106e1cc05a9d22cc60d
shejaemeric <73876114+shejaemeric@users.noreply.github.com>
2022-08-26 18:22:07 +0200

    Add files via upload

--- sort.h
+++ sort.h
@@ -1,13 +1,15 @@
-#ifndef SORT_HEADER
-#define SORT_HEADER
+#ifndef SORT_H
+#define SORT_H
+
 #include <stdio.h>
-#include <stdlib.h>
+#include "sort.h"
 
 /**
  * struct listint_s - Doubly linked list node
- * @n: Integer stored in the node
- * @next: Pointer to the next node of the list
- * @prev: Pointer to the previous node of the list
+ *
+ * @n: Integer  stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
  */
 typedef struct listint_s
 {
@@ -16,13 +18,30 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-void swap(int *arr, int idx1, int idx2);
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
+/* Prototypes for mandatory tasks */
 void bubble_sort(int *array, size_t size);
-listint_t *create_listint(const int *array, size_t size);
-void swap_list(listint_t *pn, listint_t *temp, listint_t *nxt, listint_t **h);
 void insertion_sort_list(listint_t **list);
-void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-#endif
+void selection_sort(int *array, size_t size);
+size_t partition(int array[], size_t size);
+void print_sort(int array[], size_t size, int init);
+
+/* Prototypes for advanced tasks */
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+
+/* Prototypes for print functions */
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+
+/* Prototypes for helper functions */
+void swap(int *a, int *b);
+void heapify(int *array, int end, int start, size_t size);
+
+#endif /* SORT_H */
